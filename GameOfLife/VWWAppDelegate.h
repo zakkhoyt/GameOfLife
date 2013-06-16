d3dab2eb209fac4df1f50b7a0883d3d3344948bb
Zakk Hoyt <vaporwarewolf@gmail.com>
2013-06-15 23:14:32 -0700

    Initial Commit

---
+++ GameOfLife/VWWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VWWAppDelegate.h
+//  GameOfLife
+//
+//  Created by Zakk Hoyt on 6/15/13.
+//  Copyright (c) 2013 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VWWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GameOfLifeTests/GameOfLifeTests.m
@@ -0,0 +1,36 @@
+//
+//  GameOfLifeTests.m
+//  GameOfLifeTests
+//
+//  Created by Zakk Hoyt on 6/15/13.
+//  Copyright (c) 2013 Zakk Hoyt. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GameOfLifeTests : XCTestCase
+
+@end
+
+@implementation GameOfLifeTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
